f001cc7acc41a16c770ac5840c771ef1b479a26e
luo0412 <1095847440@qq.com>
2022-03-22 07:21:50 +0800

    Initial commit

---
+++ .umirc.js
@@ -0,0 +1,62 @@
+import { defineConfig } from 'umi';
+
+const { getEntry } = require('./mpa.utils')
+const { CONFIG } = require('./mpa.config')
+
+const mpaEntries = getEntry(CONFIG.entry);
+
+const routes = [
+  {
+    path: '/', 
+    component: mpaEntries.entry[0].component
+  },
+  ...mpaEntries.entry
+]
+console.log(routes)
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  // routes: [
+  //   { 
+  //     path: '/', 
+  //     component: '../../__demo/page1.tsx' 
+  //   },
+  //   { 
+  //     path: '/page2', 
+  //     component: '../../__demo/page2.jsx' 
+  //   },
+  // ],
+  routes,
+  // publicPath: '/tpl-mpa-umi3-react17/',
+  publicPath: './',
+  outputPath: 'docs',
+  mfsu: { 
+    // production: { output: '.mfsu-production' } 
+  },
+  // fastRefresh: {},
+  
+  // mpa: {},
+  // exportStatic: {},
+
+  chainWebpack(config, { env, webpack, createCSSRule }) {
+    // 设置 alias
+    // config.resolve.alias.set('@', 'src');
+
+
+    // 入口配置 @todo
+    // config
+    // .entry('index')
+    //   .add('./__demo/page1');
+    
+    // config.entry('page2')
+    //     .add('./__demo/page2.jsx')
+    //     .end();
+
+    // 删除 umi 内置插件
+    // config.plugins.delete('progress');
+    // config.plugins.delete('friendly-error');
+    // config.plugins.delete('copy');
+  },
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+# umi project
+
+
+# 常见问题 @bug
+
+- 修改entry, 争取打包出来MPA @todo
+- 打包路径publicPath有问题
