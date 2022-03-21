b9f4c5d00fe706c2185dd8d12be44000a1674030
luo0412 <1095847440@qq.com>
2022-03-21 23:35:37 +0000

    adjust

--- .umirc.js
+++ .umirc.js
@@ -33,26 +33,26 @@ export default defineConfig({
   // publicPath: './',
   outputPath: 'docs',
   mfsu: {
-    // production: { output: '.mfsu-production' }
+    //   // production: { output: '.mfsu-production' }
   },
   // fastRefresh: {},
 
   // mpa: {},
   // exportStatic: {},
 
-  chainWebpack(config, { env, webpack, createCSSRule }) {
-    // 设置 alias
-    // config.resolve.alias.set('@', 'src');
-    // 入口配置 @todo
-    // config
-    // .entry('index')
-    //   .add('./__demo/page1');
-    // config.entry('page2')
-    //     .add('./__demo/page2.jsx')
-    //     .end();
-    // 删除 umi 内置插件
-    // config.plugins.delete('progress');
-    // config.plugins.delete('friendly-error');
-    // config.plugins.delete('copy');
-  },
+  // chainWebpack(config, { env, webpack, createCSSRule }) {
+  //   // 设置 alias
+  //   // config.resolve.alias.set('@', 'src');
+  //   // 入口配置 @todo
+  //   // config
+  //   // .entry('index')
+  //   //   .add('./__demo/page1');
+  //   // config.entry('page2')
+  //   //     .add('./__demo/page2.jsx')
+  //   //     .end();
+  //   // 删除 umi 内置插件
+  //   // config.plugins.delete('progress');
+  //   // config.plugins.delete('friendly-error');
+  //   // config.plugins.delete('copy');
+  // },
 });
